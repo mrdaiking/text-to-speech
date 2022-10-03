a7c7b6e044b7f787911c7312e9ee678e11408e78
Cuong <cuong@blayn.co.jp>
2022-10-03 16:31:45 +0900

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import TabNav from './src/components/TabNav';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <StatusBar style="auto" />
+      <NavigationContainer>
+        <TabNav />
+      </NavigationContainer>
     </View>
   );
 }
